2751463e03c239855ef2a2e9452a80bb60b6149c
burakgunce <brk.gnc9@hotmail.com>
2024-04-27 23:49:36 +0300

    payment service description was completed

---
+++ MicroserviceTrainings/Services/PaymentService/OrderStartedIntegrationEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using MicroserviceTrainings.Eventbus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroserviceTrainings.Services.PaymentService
+{
+    internal class OrderStartedIntegrationEventHandler
+    {
+        // bunun tetiklenme sebebi baseeventbus taki processevent metodunun en altındaki handle kısmı
+    }
+    public class OrderStartedIntegrationEventHandler : IIntegrationEventHandler<OrderStartedIntegrationEvent>
+    {
+        private readonly IConfiguration configuration; // Uygulama yapılandırmasını temsil eden bir özellik.
+        private readonly IEventBus eventBus; // Olay otobüsünü temsil eden bir özellik.
+        private readonly ILogger<OrderStartedIntegrationEventHandler> logger; // Günlük oluşturmayı sağlayan bir özellik.
+
+        public OrderStartedIntegrationEventHandler(IConfiguration configuration, IEventBus eventBus, ILogger<OrderStartedIntegrationEventHandler> logger)
+        {
+            // Bağımlılıkları enjekte eden bir yapılandırıcı.
+            // IConfiguration, IEventBus ve ILogger özelliklerini alır ve ilgili alanlara atar.
+        }
+
+        public Task Handle(OrderStartedIntegrationEvent @event)
+        {
+            // Olayı işleyen bir yöntem.
+            // Bu yöntemde bir ödeme işlemi simüle edilir.
+            // Ödeme başarılı mı başarısız mı olduğu, yapılandırmadan alınır.
+            // Başarılı veya başarısız olduğuna bağlı olarak uygun bir entegrasyon olayı oluşturulur.
+            // Olay günlüğe kaydedilir ve olay otobüsüne yayınlanır.
+            // İşlem tamamlandığında bir tamamlama belirtisi döndürülür.
+        }
+    }
+
+}
