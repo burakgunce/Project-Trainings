c0b97a2624615d18ebcb99faffaa18a0484df722
burakgunce <brk.gnc9@hotmail.com>
2024-04-30 16:44:22 +0300

    seed data with bogus and some handler working was completed

---
+++ BlazorForumTrainings/Application/LoginUserCommandHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorForumTrainings.Application
+{
+    internal class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, LoginUserViewModel>
+    {
+        private readonly IUserRepository userRepository; // Kullanıcı veritabanı işlemlerini gerçekleştirmek için repository bağımlılığı
+        private readonly IMapper mapper; // Veri aktarımı için nesne eşleştirme işlemlerini gerçekleştirmek için mapper bağımlılığı
+        private readonly IConfiguration configuration; // Yapılandırma bilgilerine erişmek için bağımlılık
+
+        public LoginUserCommandHandler(IUserRepository userRepository, IMapper mapper, IConfiguration configuration)
+        {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+            this.configuration = configuration;
+        }
+
+        // Giriş işlemini gerçekleştiren metot
+        public async Task<LoginUserViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            // Kullanıcıyı e-posta adresine göre veritabanından getir
+            var dbUser = await userRepository.GetSingleAsync(i => i.EmailAddress == request.EmailAddress);
+
+            // Eğer kullanıcı bulunamazsa hata fırlat
+            if (dbUser == null)
+                throw new DatabaseValidationException("User not found!");
+
+            // Kullanıcının parolasını şifrele ve veritabanındakiyle karşılaştır
+            var pass = PasswordEncryptor.Encrypt(request.Password);
+            if (dbUser.Password != pass)
+                throw new DatabaseValidationException("Password is wrong!");
+
+            // Eğer kullanıcının e-posta adresi onaylanmamışsa hata fırlat
+            if (!dbUser.EmailConfirmed)
+                throw new DatabaseValidationException("Email address is not confirmed yet!");
+
+            // Kullanıcı bilgilerini bir view model'e eşle ve token oluştur
+            var result = mapper.Map<LoginUserViewModel>(dbUser);
+
+            var claims = new Claim[]
+            {
+            // JWT token için talepleri oluştur
+            new Claim(ClaimTypes.NameIdentifier, dbUser.Id.ToString()),
+            new Claim(ClaimTypes.Email, dbUser.EmailAddress),
+            new Claim(ClaimTypes.Name, dbUser.UserName),
+            new Claim(ClaimTypes.GivenName, dbUser.FirstName),
+            new Claim(ClaimTypes.Surname, dbUser.LastName)
+            };
+
+            // Token oluştur ve view modele ekle
+            result.Token = GenerateToken(claims);
+
+            return result;
+        }
+
+        // JWT token oluşturma işlemini gerçekleştiren metot
+        private string GenerateToken(Claim[] claims)
+        {
+            // Secret key ve signing credentials oluştur
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["AuthConfig:Secret"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // Token'in geçerlilik süresini belirle
+            var expiry = DateTime.Now.AddDays(10);
+
+            // Token'i oluştur
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: expiry,
+                signingCredentials: creds,
+                notBefore: DateTime.Now
+            );
+
+            // Token'i string olarak dön
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+
+}
---
+++ BlazorForumTrainings/Persistence/SeedData.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorForumTrainings.Persistence
+{
+    internal class SeedData
+    {
+        // Rastgele kullanıcılar oluşturur ve döndürür
+        private static List<User> GetUsers()
+        {
+            var result = new Faker<User>("tr")
+                .RuleFor(i => i.Id, i => Guid.NewGuid()) // Rastgele bir GUID oluşturur
+                .RuleFor(i => i.CreateDate, i => i.Date.Between(DateTime.Now.AddDays(-100), DateTime.Now)) // Oluşturulma tarihini belirler
+                .RuleFor(i => i.FirstName, i => i.Person.FirstName) // Rastgele bir isim oluşturur
+                .RuleFor(i => i.LastName, i => i.Person.LastName) // Rastgele bir soyisim oluşturur
+                .RuleFor(i => i.EmailAddress, i => i.Internet.Email()) // Rastgele bir e-posta adresi oluşturur
+                .RuleFor(i => i.UserName, i => i.Internet.UserName()) // Rastgele bir kullanıcı adı oluşturur
+                .RuleFor(i => i.Password, i => PasswordEncryptor.Encrypt(i.Internet.Password())) // Rastgele bir şifre oluşturur ve şifreler
+                .RuleFor(i => i.EmailConfirmed, i => i.PickRandom(true, false)) // Rastgele bir onaylanmış/ onaylanmamış e-posta durumu oluşturur
+                .Generate(500); // 500 kullanıcı oluşturur
+
+            return result;
+        }
+
+        // Veritabanına başlangıç verilerini ekler
+        public async Task SeedAsync(IConfiguration configuration)
+        {
+            var dbContextBuilder = new DbContextOptionsBuilder();
+            dbContextBuilder.UseSqlServer(configuration["BlazorSozlukDbConnectionString"]); // Veritabanı bağlantı dizesini alır ve kullanır
+
+            var context = new BlazorSozlukContext(dbContextBuilder.Options); // Veritabanı bağlamını oluşturur
+
+            // Eğer veritabanında kullanıcı varsa işlemi tamamla
+            if (context.Users.Any())
+            {
+                await Task.CompletedTask;
+                return;
+            }
+
+            // Kullanıcıları al ve veritabanına ekle
+            var users = GetUsers();
+            await context.Users.AddRangeAsync(users);
+
+            // Entry ve EntryComment nesnelerini oluştur ve veritabanına ekle
+            var entries = GenerateEntries(users);
+            var comments = GenerateComments(users, entries);
+
+            await context.Entries.AddRangeAsync(entries);
+            await context.EntryComments.AddRangeAsync(comments);
+
+            // Değişiklikleri veritabanına kaydet
+            await context.SaveChangesAsync();
+        }
+
+        // Girişler oluşturur
+        private static List<Entry> GenerateEntries(List<User> users)
+        {
+            var guids = Enumerable.Range(0, 150).Select(i => Guid.NewGuid()).ToList(); // 150 adet GUID oluşturur
+            int counter = 0;
+
+            var entries = new Faker<Entry>("tr")
+                .RuleFor(i => i.Id, i => guids[counter++]) // GUID'leri girişlere atar
+                .RuleFor(i => i.CreateDate, i => i.Date.Between(DateTime.Now.AddDays(-100), DateTime.Now)) // Oluşturulma tarihlerini belirler
+                .RuleFor(i => i.Subject, i => i.Lorem.Sentence(5, 5)) // Konuları rastgele oluşturur
+                .RuleFor(i => i.Content, i => i.Lorem.Paragraph(2)) // İçerikleri rastgele oluşturur
+                .RuleFor(i => i.CreatedById, i => i.PickRandom(users.Select(u => u.Id))) // Oluşturan kullanıcıları rastgele seçer
+                .Generate(150); // 150 adet giriş oluşturur
+
+            return entries;
+        }
+
+        // Yorumlar oluşturur
+        private static List<EntryComment> GenerateComments(List<User> users, List<Entry> entries)
+        {
+            var comments = new Faker<EntryComment>("tr")
+                .RuleFor(i => i.Id, i => Guid.NewGuid()) // Rastgele bir GUID oluşturur
+                .RuleFor(i => i.CreateDate, i => i.Date.Between(DateTime.Now.AddDays(-100), DateTime.Now)) // Oluşturulma tarihlerini belirler
+                .RuleFor(i => i.Content, i => i.Lorem.Paragraph(2)) // İçerikleri rastgele oluşturur
+                .RuleFor(i => i.CreatedById, i => i.PickRandom(users.Select(u => u.Id))) // Oluşturan kullanıcıları rastgele seçer
+                .RuleFor(i => i.EntryId, i => i.PickRandom(entries.Select(e => e.Id))) // Girişleri rastgele seçer
+                .Generate(1000); // 1000 adet yorum oluşturur
+
+            return comments;
+        }
+    }
+
+}
