019df9fd9c4b4ba463b38ce1e5ec6092f5445014
burakgunce <brk.gnc9@hotmail.com>
2024-04-30 20:48:24 +0300

    working of page operations was completed

---
+++ BlazorForumTrainings/Application/GetMainPageEntriesQueryHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorForumTrainings.Application
+{
+    public class GetMainPageEntriesQueryHandler : IRequestHandler<GetMainPageEntriesQuery, PagedViewModel<GetEntryDetailViewModel>>
+    {
+        private readonly IEntryRepository _entryRepository;
+
+        // Bağımlılıkların enjekte edildiği kurucu metot.
+        public GetMainPageEntriesQueryHandler(IEntryRepository entryRepository)
+        {
+            _entryRepository = entryRepository;
+        }
+
+        // Ana sayfa girişlerini almak için kullanılan metot.
+        public async Task<PagedViewModel<GetEntryDetailViewModel>> Handle(GetMainPageEntriesQuery request, CancellationToken cancellationToken)
+        {
+            // Girişlerin sorgusu yapılır.
+            var query = _entryRepository.AsQueryable();
+
+            // Girişlerin ilişkili verileri ile birlikte alınması sağlanır.
+            query = query.Include(i => i.EntryFavorites)
+                         .Include(i => i.CreatedBy)
+                         .Include(i => i.EntryVotes);
+
+            // Girişlerin detaylarının projeksiyonu yapılır.
+            var list = query.Select(i => new GetEntryDetailViewModel()
+            {
+                Id = i.Id,
+                Subject = i.Subject,
+                Content = i.Content,
+                IsFavorited = request.UserId.HasValue && i.EntryFavorites.Any(j => j.CreatedById == request.UserId),
+                FavoritedCount = i.EntryFavorites.Count,
+                CreatedDate = i.CreateDate,
+                CreatedByUserName = i.CreatedBy.UserName,
+                VoteType =
+                    request.UserId.HasValue && i.EntryVotes.Any(j => j.CreatedById == request.UserId)
+                    ? i.EntryVotes.FirstOrDefault(j => j.CreatedById == request.UserId).VoteType
+                    : Common.ViewModels.VoteType.None
+            });
+
+            // Sayfalı bir şekilde girişler döndürülür.
+            var entries = await list.GetPaged(request.Page, request.PageSize);
+
+            return entries;
+        }
+    }
+
+}
---
+++ BlazorForumTrainings/WebApi/PagingExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorForumTrainings.WebApi
+{
+    public static class PagingExtensions
+    {
+        // IQueryable veri kümesini sayfalı bir şekilde döndüren genişletme yöntemi.
+        public static async Task<PagedViewModel<T>> GetPaged<T>(this IQueryable<T> query, int currentPage, int pageSize) where T : class
+        {
+            // Veri kümesinin toplam öğe sayısını alır.
+            var count = await query.CountAsync();
+
+            // Sayfalama bilgilerini içeren bir Page nesnesi oluşturulur.
+            Page paging = new(currentPage, pageSize, count);
+
+            // Sayfalı veri kümesi, belirli sayfa ve sayfa boyutuna göre alınır.
+            var data = await query.Skip(paging.Skip).Take(paging.PageSize).AsNoTracking().ToListAsync();
+
+            // Sayfalı veri kümesi ve sayfalama bilgileriyle bir PagedViewModel oluşturulur ve döndürülür.
+            var result = new PagedViewModel<T>(data, paging);
+            return result;
+        }
+    }
+
+}
