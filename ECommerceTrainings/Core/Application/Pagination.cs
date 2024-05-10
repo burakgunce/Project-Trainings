0e926d48a6e7ab6404f3f99707f48e8934bc442c
burakgunce <brk.gnc9@hotmail.com>
2024-05-10 18:11:03 +0300

    validation filter and pagination working was added

---
+++ ECommerceTrainings/Core/Application/Pagination.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceTrainings.Core.Application
+{
+    internal class Pagination
+    {
+        /*
+         * 
+         * public async Task<GetAllProductQueryResponse> Handle(GetAllProductQueryRequest request, CancellationToken cancellationToken)
+{
+    // Tüm ürünlerin alınması işlemi başlatılıyor, bu bir sorgu işleyicisidir.
+    _logger.LogInformation("get all products");
+
+    // Toplam ürün sayısını almak için tüm ürünlerin sayısını sayar.
+    var totalProductCount = _productReadRepository.GetAll().Count();
+
+    // Ürünlerin sayfalı bir listesi alınır.
+    var products = _productReadRepository.GetAll(false) // İzleme devre dışı bırakılmış ürünler alınıyor.
+        .Skip(request.Page * request.Size) // Sayfa numarasına göre ürünlerin belirli bir kısmı atlanır.
+        .Take(request.Size) // Sayfa boyutu kadar ürün alınır.
+        .Select(p => new // Her üründen belirli alanlar seçilir.
+        {
+            p.Id,
+            p.Name,
+            p.Stock,
+            p.Price,
+            p.CreatedDate,
+            p.UpdatedDate
+        })
+        .ToList(); // Seçilen ürünler bir liste olarak alınır.
+
+    // Sorgu sonucunu temsil eden bir nesne oluşturulur ve geri döndürülür.
+    return new GetAllProductQueryResponse()
+    {
+        Products = products, // Ürünler
+        TotalProductCount = totalProductCount // Toplam ürün sayısı
+    };
+}
+
+         * 
+         */
+    }
+}
---
+++ ECommerceTrainings/Infrastructure/Infrastruct/ValidationFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceTrainings.Infrastructure.Infrastruct
+{
+    internal class ValidationFilter
+    {
+        /* default modelstate yerine fluent validation artık kontrolleri yapıyor
+         * 
+         * 
+         * public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+{
+    // Model durumu geçerli değilse işleme devam edilmez ve hata sonucu döndürülür.
+    if (!context.ModelState.IsValid)
+    {
+        // Model durumu geçersiz olan hataların bir listesi oluşturulur.
+        var errors = context.ModelState
+               .Where(x => x.Value.Errors.Any())
+               .ToDictionary(e => e.Key, e => e.Value.Errors.Select(e => e.ErrorMessage))
+               .ToArray();
+
+        // Hata sonucu BadRequestObjectResult tipinde bir sonuçla değiştirilir ve işleme devam edilmez.
+        context.Result = new BadRequestObjectResult(errors);
+        return;
+    }
+
+    // Eğer model durumu geçerli ise, işlem devam eder ve bir sonraki aksiyon çağrılır.
+    await next();
+}
+
+
+        Bu metot, bir HTTP isteğinin, isteği karşılayan bir aksiyon metodu içerisindeki modelin doğruluğunu kontrol eder. Eğer model durumu geçerli değilse, modeldeki hatalar bir liste olarak toplanır ve BadRequestObjectResult tipinde bir sonuçla isteğe cevap olarak döndürülür. Eğer model durumu geçerli ise, isteğin işlenmesi devam eder ve bir sonraki aksiyon metodu çağrılır. Bu tür bir doğrulama genellikle, gelen verilerin uygunluğunu kontrol etmek için kullanılır ve istemcilerin hatalı veri göndermelerini önler.
+         * 
+         * 
+         */
+    }
+}
