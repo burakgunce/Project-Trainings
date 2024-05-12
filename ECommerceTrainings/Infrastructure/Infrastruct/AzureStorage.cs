a4a9da83392385be7f3568ae3c21045bdacf89a4
burakgunce <brk.gnc9@hotmail.com>
2024-05-12 23:12:40 +0300

    different storage types working has been completed

---
+++ ECommerceTrainings/Infrastructure/Infrastruct/AzureStorage.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceTrainings.Infrastructure.Infrastruct
+{
+    internal class AzureStorage
+    {
+        /*
+         * public class AzureStorage : Storage, IAzureStorage
+{
+    // Blob servis istemcisini tutar
+    readonly BlobServiceClient _blobServiceClient;
+    // Blob konteyner istemcisini tutar
+    BlobContainerClient _blobContainerClient;
+
+    // Constructor: Yapılandırmadan Blob hizmet istemcisini oluşturur
+    public AzureStorage(IConfiguration configuration)
+    {
+        _blobServiceClient = new(configuration["Storage:Azure"]);
+    }
+
+    // Dosyayı siler
+    public async Task DeleteAsync(string containerName, string fileName)
+    {
+        // Blob konteyner istemcisini alır
+        _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+        // Blob istemcisini alır ve dosyayı siler
+        BlobClient blobClient = _blobContainerClient.GetBlobClient(fileName);
+        await blobClient.DeleteAsync();
+    }
+
+    // Belirli bir konteynerdeki dosyaları listeler
+    public List<string> GetFiles(string containerName)
+    {
+        // Blob konteyner istemcisini alır ve dosyaları listeler
+        _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+        return _blobContainerClient.GetBlobs().Select(b => b.Name).ToList();
+    }
+
+    // Belirli bir dosyanın belirli bir konteynerde bulunup bulunmadığını kontrol eder
+    public bool HasFile(string containerName, string fileName)
+    {
+        // Blob konteyner istemcisini alır ve dosyanın varlığını kontrol eder
+        _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+        return _blobContainerClient.GetBlobs().Any(b => b.Name == fileName);
+    }
+
+    // Dosyaları yükler
+    public async Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string containerName, IFormFileCollection files)
+    {
+        // Blob konteyner istemcisini alır ve varsa oluşturur
+        _blobContainerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+        await _blobContainerClient.CreateIfNotExistsAsync();
+        await _blobContainerClient.SetAccessPolicyAsync(PublicAccessType.BlobContainer);
+
+        // Yüklenen dosyaların bilgilerini tutar
+        List<(string fileName, string pathOrContainerName)> datas = new();
+        // Her bir dosya için yükleme işlemi yapar
+        foreach (IFormFile file in files)
+        {
+            // Dosya ismini yeniden adlandırır
+            string newFileName = await FileRenameAsync(containerName, file.Name, HasFile);
+            // Blob istemcisini alır ve dosyayı yükler
+            BlobClient blobClient = _blobContainerClient.GetBlobClient(newFileName);
+            await blobClient.UploadAsync(file.OpenReadStream());
+            // Dosya bilgilerini listeye ekler
+            datas.Add((newFileName, $"{containerName}/{newFileName}"));
+        }
+        // Yüklenen dosyaların bilgilerini döndürür
+        return datas;
+    }
+}
+
+         * 
+         * 
+         */
+    }
+}
---
+++ ECommerceTrainings/Infrastructure/Infrastruct/LocalStorage.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceTrainings.Infrastructure.Infrastruct
+{
+    internal class LocalStorage
+    {
+        /*
+         * public class LocalStorage : Storage, ILocalStorage
+{
+    // Web ana bilgisayar ortamı arayüzü
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    // Constructor: Web ana bilgisayar ortamı arayüzünü enjekte eder
+    public LocalStorage(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    // Dosyayı siler
+    public async Task DeleteAsync(string path, string fileName)
+        => File.Delete($"{path}\\{fileName}");
+
+    // Belirli bir dizindeki dosyaları listeler
+    public List<string> GetFiles(string path)
+    {
+        DirectoryInfo directory = new(path);
+        return directory.GetFiles().Select(f => f.Name).ToList();
+    }
+
+    // Belirli bir dizinde belirli bir dosyanın olup olmadığını kontrol eder
+    public bool HasFile(string path, string fileName)
+        => File.Exists($"{path}\\{fileName}");
+
+    // Dosyayı kopyalar
+    async Task<bool> CopyFileAsync(string path, IFormFile file)
+    {
+        try
+        {
+            // Dosya akışını oluşturur ve dosyayı kopyalar
+            await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None,
+                1024 * 1024, useAsync: false);
+
+            await file.CopyToAsync(fileStream);
+            await fileStream.FlushAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            // Hata durumunda loglama yapar ve hatayı yeniden fırlatır
+            // TODO: Loglama işlemi yapılabilir
+            throw ex;
+        }
+    }
+
+    // Dosyayı yükler
+    public async Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string path, IFormFileCollection files)
+    {
+        // Yükleme yolunu oluşturur
+        string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, path);
+        // Yükleme dizini yoksa oluşturur
+        if (!Directory.Exists(uploadPath))
+        {
+            Directory.CreateDirectory(uploadPath);
+        }
+
+        // Yüklenen dosyaların bilgilerini tutar
+        List<(string fileName, string path)> datas = new();
+        // Her bir dosya için yükleme işlemi yapar
+        foreach (IFormFile file in files)
+        {
+            // Dosya ismini yeniden adlandırır
+            string newFileName = await FileRenameAsync(uploadPath, file.Name, HasFile);
+
+            // Dosyayı kopyalar ve yüklenen dosyanın bilgilerini listeye ekler
+            await CopyFileAsync($"{uploadPath}\\{newFileName}", file);
+            datas.Add((newFileName, $"{uploadPath}\\{newFileName}"));
+        }
+
+        // Yüklenen dosyaların bilgilerini döndürür
+        return datas;
+    }
+}
+
+         * 
+         * 
+         */
+    }
+}
