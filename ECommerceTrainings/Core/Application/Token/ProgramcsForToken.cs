3dbc9d4e5ea4cb8444b517aecbac715451e3bb05
burakgunce <brk.gnc9@hotmail.com>
2024-05-13 23:42:19 +0300

    jwt working was completed

---
+++ ECommerceTrainings/Core/Application/Token/ProgramcsForToken.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceTrainings.Core.Application.Token
+{
+    internal class ProgramcsForToken
+    {
+        /*
+         * 
+         * builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer("Admin", options =>
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidAudience = builder.Configuration["Token:Audience"],
+            ValidIssuer = builder.Configuration["Token:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])),
+            LifetimeValidator = (notBefore, expires, securityToken, validationParameters) => expires != null ? expires > DateTime.UtcNow : false,
+
+            NameClaimType = ClaimTypes.Name // JWT'den ad değerini almak için
+        };
+    });
+         * 
+         */
+    }
+}
---
+++ ECommerceTrainings/Core/Application/Token/TokenHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceTrainings.Core.Application.Token
+{
+    internal class TokenHandler
+    {
+        /*
+         * 
+         * public class TokenHandler : ITokenHandler
+{
+    readonly IConfiguration _configuration;
+
+    // Constructor: Configuration enjekte edilir
+    public TokenHandler(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    // Erişim belgesi oluşturur
+    public Application.DTOs.Token CreateAccessToken(int second, AppUser user)
+    {
+        Application.DTOs.Token token = new();
+
+        // Güvenlik anahtarı oluşturulur
+        SymmetricSecurityKey securityKey = new(Encoding.UTF8.GetBytes(_configuration["Token:SecurityKey"]));
+
+        // İmzalama kimlik bilgileri oluşturulur
+        SigningCredentials signingCredentials = new(securityKey, SecurityAlgorithms.HmacSha256);
+
+        // Token'in son kullanma tarihini belirler
+        token.Expiration = DateTime.UtcNow.AddSeconds(second);
+        
+        // JWT güvenlik belgesi oluşturulur
+        JwtSecurityToken securityToken = new(
+            audience: _configuration["Token:Audience"],
+            issuer: _configuration["Token:Issuer"],
+            expires: token.Expiration,
+            notBefore: DateTime.UtcNow
+            );
+
+        // JWT güvenlik belgesi yazıcı oluşturulur
+        JwtSecurityTokenHandler tokenHandler = new();
+        // Erişim belgesini yazılı bir JWT belgesine dönüştürür
+        token.AccessToken = tokenHandler.WriteToken(securityToken);
+
+        return token;
+    }
+}
+         * 
+         */
+    }
+}
