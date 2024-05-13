0e1d8010596ba633c1e7d280ade0a8d5366e3efb
burakgunce <brk.gnc9@hotmail.com>
2024-05-13 16:40:17 +0300

    product images cqrs woring was added

---
+++ ECommerceTrainings/Core/Application/GetProductImagesCQRS.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceTrainings.Core.Application
+{
+    internal class GetProductImagesCQRS
+    {
+        /*
+         * public class GetProductImagesQueryRequest : IRequest<List<GetProductImagesQueryResponse>>
+{
+    // Ürün ID'si
+    public string Id { get; set; }
+}
+
+public class GetProductImagesQueryResponse
+{
+    // Resmin yol bilgisi
+    public string Path { get; set; }
+    // Resmin dosya adı
+    public string FileName { get; set; }
+    // Resmin ID'si
+    public Guid Id { get; set; }
+}
+
+public class GetProductImagesQueryHandler : IRequestHandler<GetProductImagesQueryRequest, List<GetProductImagesQueryResponse>>
+{
+    readonly IProductReadRepository _productReadRepository; // Ürün okuma deposu
+    readonly IConfiguration configuration; // Uygulama yapılandırması
+
+    // Constructor: Ürün okuma deposu ve yapılandırma enjekte edilir
+    public GetProductImagesQueryHandler(IProductReadRepository productReadRepository, IConfiguration configuration)
+    {
+        _productReadRepository = productReadRepository;
+        this.configuration = configuration;
+    }
+
+    // İsteği işler
+    public async Task<List<GetProductImagesQueryResponse>> Handle(GetProductImagesQueryRequest request, CancellationToken cancellationToken)
+    {
+        // Ürünü bulur
+        Domain.Entities.Product? product = await _productReadRepository.Table.Include(p => p.ProductImageFiles)
+               .FirstOrDefaultAsync(p => p.Id == Guid.Parse(request.Id));
+        
+        // Eğer ürün bulunduysa, ürün resimlerini dönüştürür
+        return product?.ProductImageFiles.Select(p => new GetProductImagesQueryResponse
+        {
+            // Resmin yolunu oluşturur
+            Path = $"{configuration["BaseStorageUrl"]}/{p.Path}",
+            // Resmin dosya adını belirler
+            FileName = p.FileName,
+            // Resmin ID'sini belirler
+            Id = p.Id
+        }).ToList();
+    }
+}
+
+         * 
+         * 
+         */
+    }
+}
---
+++ ECommerceTrainings/Core/Application/RemoveProductImageCQRS.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceTrainings.Core.Application
+{
+    internal class RemoveProductImageCQRS
+    {
+        /*
+         * public class RemoveProductImageCommandRequest : IRequest<RemoveProductImageCommandResponse>
+{
+    // Ürün ID'si
+    public string Id { get; set; }
+    // Resim ID'si (isteğe bağlı)
+    public string? ImageId { get; set; }
+}
+
+public class RemoveProductImageCommandResponse
+{
+}
+
+public class RemoveProductImageCommandHandler : IRequestHandler<RemoveProductImageCommandRequest, RemoveProductImageCommandResponse>
+{
+    readonly IProductReadRepository _productReadRepository; // Ürün okuma deposu
+    readonly IProductWriteRepository _productWriteRepository; // Ürün yazma deposu
+
+    // Constructor: Ürün okuma ve yazma depoları enjekte edilir
+    public RemoveProductImageCommandHandler(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+    {
+        _productReadRepository = productReadRepository;
+        _productWriteRepository = productWriteRepository;
+    }
+
+    // İsteği işler
+    public async Task<RemoveProductImageCommandResponse> Handle(RemoveProductImageCommandRequest request, CancellationToken cancellationToken)
+    {
+        // Ürünü bulur
+        Domain.Entities.Product? product = await _productReadRepository.Table.Include(p => p.ProductImageFiles)
+            .FirstOrDefaultAsync(p => p.Id == Guid.Parse(request.Id));
+
+        // Ürün resmini bulur
+        Domain.Entities.ProductImageFile? productImageFile = product?.ProductImageFiles.FirstOrDefault(p => p.Id == Guid.Parse(request.ImageId));
+
+        // Eğer resim bulunduysa, üründen kaldırır
+        if (productImageFile != null)
+            product?.ProductImageFiles.Remove(productImageFile);
+
+        // Değişiklikleri kaydeder
+        await _productWriteRepository.SaveAsync();
+
+        // Boş bir yanıt döndürür
+        return new();
+    }
+}
+
+         * 
+         */
+    }
+}
