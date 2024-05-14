ad527c129c97e9803c0923a43ad8d4591f5882bc
burakgunce <brk.gnc9@hotmail.com>
2024-05-14 23:02:55 +0300

    some refactor working was added

---
+++ ECommerceTrainings/Core/Application/DTOs/CreateUserDTOs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceTrainings.Core.Application.DTOs
+{
+    internal class CreateUserDTOs
+    {
+        /*
+         * public class CreateUser
+    {
+        public string NameSurname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+
+        public class CreateUserResponse
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+    }
+         * 
+         * 
+         */
+    }
+}
