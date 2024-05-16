69272b9ee09316925ca32b698fa96810f76ebd9d
burakgunce <brk.gnc9@hotmail.com>
2024-05-16 14:54:07 +0300

    .

---
+++ generictrain/MyList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generictrain
+{
+    public class MyList<T>
+    {
+        public List<T> list = new List<T>();
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            list.Remove(item);
+        }
+
+        public int Count()
+        {
+            int count = list.Count;
+            return count;
+        }
+
+        private T[] array = new T[0];
+
+        public void Add2(T item)
+        {
+            T[] newArray = new T[array.Length + 1];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            newArray[array.Length] = item;
+            array = newArray;
+        }
+
+        public void Remove2(T item)
+        {
+            if (array.Contains(item))
+            {
+                T[] newArray = new T[array.Length - 1];
+                int newIndex = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (!array[i].Equals(item))
+                    {
+                        newArray[newIndex] = array[i];
+                        newIndex++;
+                    }
+                }
+                array = newArray;
+            }
+        }
+
+        public int Count2()
+        {
+            return array.Length;
+        }
+
+    }
+}
---
+++ generictrain/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace generictrain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> toplam = new MyList<int>();
+            toplam.Add(1);
+            toplam.Add(2);
+
+            MyList<string> cumle = new MyList<string>();
+            cumle.Add("a");
+            cumle.Add("b");
+
+            Console.WriteLine(toplam.Count());
+            Console.WriteLine(cumle.Count());
+
+            MyList<int> toplam2 = new MyList<int>();
+            toplam2.Add2(1);
+            toplam2.Add2(2);
+            toplam2.Add2(3);
+
+            MyList<string> cumle2 = new MyList<string>();
+            cumle2.Add2("a");
+            cumle2.Add2("b");
+            cumle2.Add2("c");
+
+            Console.WriteLine(toplam2.Count2());
+            Console.WriteLine(cumle2.Count2());
+
+            
+        }
+    }
+}
